638dc8eae6be10505519516c7f98ed87df7d9caf
Christophe H <65390576+christophe-havard-sonarsource@users.noreply.github.com>
2022-02-01 16:31:48 +0100

    sqlQuery

--- vuln1.php
+++ vuln1.php
@@ -8,7 +8,15 @@ class MyAwesomeClass
     protected $connection;
     protected $httpUrl = "https://example.domain?user=user&password=65DBGgwe4uazdWQA" // Sensitive
 
-    
+    public function sqlQuery1(Request $request)
+    {
+        $userId = $request->get('id');
+        $sql = "SELECT email FROM user WHERE id='$userId'";
+        $statement = $this->connection->prepare($sql);
+        $statement->execute();
+        $username = $statement->fetchColumn();
+        return $this->json(['email' => $username]);
+    }
 
     public function newVulnFunction(Request $request)
     {
