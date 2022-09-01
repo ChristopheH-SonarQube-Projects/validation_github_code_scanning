1c5c51d8a14cfe9f722c6659297620d2c1d992a3
Christophe H <65390576+christophe-havard-sonarsource@users.noreply.github.com>
2022-09-01 10:48:14 +0200

    Create vuln.php

---
+++ vuln.php
@@ -0,0 +1,25 @@
+<?php 
+
+use Symfony\Component\HttpFoundation\Request;
+
+class Controller
+{
+    /**
+     * @var \Doctrine\DBAL\Connection
+     */
+    protected $connection;
+    protected $httpUrl = "https://example.domain?user=user&password=65DBGgwe4uazdWQA" // Sensitive
+
+    
+
+    public function sqlQuery1(Request $request)
+    {
+        $userId = $request->get('id');
+        $sql = "SELECT email FROM user WHERE id='$userId'";
+        $statement = $this->connection->prepare($sql);
+        $statement->execute();
+        $username = $statement->fetchColumn();
+        return $this->json(['email' => $username]);
+    }
+}
+ ?>
